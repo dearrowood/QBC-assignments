4bcd558416d388bc489a13d8fe840923dc81d185
dearrowood <32168534+dearrowood@users.noreply.github.com>
2017-09-22 14:26:16 -0700

    Uploaded bootcamp_exercises_2.R
    Uploaded initial file with exercise prompts

---
+++ bootcamp_exercises_2.R
@@ -0,0 +1,49 @@
+##########################################
+##########################################
+#Bootcamp Exercise 2: Plotting & Analysis#
+##########################################
+##########################################
+
+############
+#Exercise 1#
+############
+
+# Write a function called get_heights to draw a sample of individuals
+# (100 or 1000) from the population.
+
+############
+#Exercise 2#
+############
+
+# Within the above function, compute the average height from the heights vector.
+
+############
+#Exercise 3#
+############
+
+# Make the function return the average height
+
+############
+#Exercise 4#
+############
+
+# Use a for loop to call get_heights function 100 times, taking a sample of size
+# 100 from the population. Save the mean height from each replicate in a vector called
+# mean_heights_100
+
+############
+#Exercise 5#
+############
+
+# Use a for loop to call get_heights function 1000 times, taking a sample of size
+# 100 from the population. Save the mean height from each replicate in a vector called
+# mean_heights_1000
+
+############
+#Exercise 6#
+############
+
+# Plot a historgram of the distribution off the average heights for your sample size of 100 and
+# 1000 individuals. The two sets of data should be plotted on the same axes. Add a legend. label
+# the axes. Plot the data from the 100 samples in red and the data from the 1000 samples in blue.
+# Your plot should look something like the one shown in the exercise directions.
